a4d949b5a86cb08f50c96486e7c00c2ce59bfff1
Matt Adams <60856362+Mern2020@users.noreply.github.com>
2020-07-26 17:26:50 -0500

    added function to remove friendCard, still working on getting it to work.

--- src/App.js
+++ src/App.js
@@ -5,16 +5,29 @@ import friends from "./friends.json";
 import "./App.css";
 
 class App extends React.Component {
-  state = {
+  constructor(props){
+    super(props)
+  
+  this.state = {
     friends
+  };
   }
+  removeFriend = id => {
+    
+    const friends = this.state.friends.id.filter(friend => friend.id !== id);
+    // set friends to a filter of each id, passing each item of the array's ID not equal to it's own ID
+
+    this.setState({ friends })
+  };
+
   
   render(){
   return (
     <Wrapper>
       <h1 className="title">Friends List</h1>
-      {this.state.friends.map(friend => 
+      {this.state.friends.map(friend => (
       <FriendCard
+        removeFriend = {this.removeFriend}
         id={friend.id}
         key={friend.id}
         name={friend.name}
@@ -22,12 +35,12 @@ class App extends React.Component {
         occupation={friend.occupation}
         location={friend.location}
         />
-      )}
+      ))}
     </Wrapper>
     );
-  }
+    }
   
-}
+  }
 
 
 export default App;
